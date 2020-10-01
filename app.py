dbfb6993c9faa4b5e258a3745b985ff09520c35b
MattB650 <69167336+MattB650@users.noreply.github.com>
2020-09-30 23:36:14 -0500

    Update app.py
    Fixed Dropdown box feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Importing Necessary Packages
+# Importing Necessary Packages
 import pandas as pd
 import plotly.express as px
 import os
@@ -7,8 +7,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 
-
-
 styles = {
     'pre': {
         'border': 'thin lightgrey solid',
@@ -16,48 +14,45 @@ styles = {
     }
 }
 
+sheet = pd.read_csv('~/Desktop/College.Draft.Project/Combined.Draft.csv', index_col=0, encoding='cp1252')
 
+# Remove Rows Where WS is null (Did not play in NBA) (Old Idea)
+# sheet = sheet[sheet['WS'].notna()]
 
-sheet = pd.read_csv('Combined.Draft.csv', index_col=0, encoding='cp1252')
-
-#Remove Rows Where WS is null (Did not play in NBA) (Old Idea)
-#sheet = sheet[sheet['WS'].notna()]
-
-#Remove Rows Where FIC is null(Did not have pre-draft season that met requirements)
+# Remove Rows Where FIC is null(Did not have pre-draft season that met requirements)
 sheet = sheet.query('FIC!=0')
 sheet = sheet.query('VA!=0')
 
-choice = sheet[['FIC','VA']]
-
-#Rename Z-Score Column
+# Rename Z-Score Column
 sheet['Adjusted Draft Score'] = sheet['Z-Score']
 
-#Set Parameters for Scatter Plot
+choices = sheet[['FIC', 'VA']]
 
 
-#Set Style
+# Set Style
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-#Initialize Dash Server
+# Initialize Dash Server
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-#Set App Layout
+# Set App Layout
 
 app.layout = html.Div([
     dcc.Graph(
-        id='scat'
-        #figure=fig
+        id='scat',
+        animate=True
     ),
-    dcc.Dropdown(id='dropdown',
-        options=[
-            {'label': choice, 'value': choice} for choice in choices.unique()
-            
-        ],
-        value='FIC'
-    ),
-           
+    dcc.Dropdown(id='box',
+                 options=[
+                     {'label': choice, 'value': choice} for choice in choices
+
+
+                 ],
+                 value='FIC'
+
+                 ),
 
     html.Div(className='row', children=[
         html.Div([
@@ -67,46 +62,69 @@ app.layout = html.Div([
             """),
             html.Pre(id='hover-data', style=styles['pre'])
         ], className='three columns'),
-        
+
         html.Div([
             dcc.Markdown("""
                 **Description**
-            
-               
+
+
                 """),
-                html.Pre(id='description', style=styles['pre'])
-            ], className='three columns')
-        ])
+            html.Pre(id='description', style=styles['pre'])
+        ], className='three columns')
     ])
+])
 
 
 @app.callback(
-    Output('scat', 'figure'),
-    Input('dropdown', 'value'))
-
+    dash.dependencies.Output(component_id='scat', component_property='figure'),
+    [dash.dependencies.Input(component_id='box', component_property='value')])
 def update_plot(a_value):
-    
     if a_value == 'FIC':
-        x = 'FIC'
-    else a_value == 'VA':
-        x='VA'     
-    
-    fig = px.scatter(sheet, x=x, y='Adjusted Draft Score', color='Adjusted Draft Score',
-                 title='NBA 2010-15 Drafts: Floor Impact Counter vs Adjusted Draft Score',
-                 hover_data=(['Player', 'Pos', 'Year', 'FIC', 'Adjusted Draft Score']))
 
-    fig.update_traces(mode='markers', marker_size=13)
+        fig = px.scatter(sheet, x='FIC', y='Adjusted Draft Score', color='Adjusted Draft Score',
+                         title='NBA 2010-15 Drafts: Floor Impact Counter vs Adjusted Draft Score',
+                         hover_data=(['Player', 'Pos', 'Year', 'FIC', 'Adjusted Draft Score']))
 
-    fig.update_layout(
-        hoverlabel=dict(
-            bgcolor="white",
-            font_size=16,
-            font_family="Rockwell"
+        fig.update_traces(mode='markers', marker_size=13)
+
+        fig.update_layout(
+            hoverlabel=dict(
+                bgcolor="white",
+                font_size=16,
+                font_family="Rockwell"
+            )
         )
-    )
-    return fig
+        fig.update_xaxes(range=[0, 800])
+        return fig
+    elif a_value == 'VA':
+        fig = px.scatter(sheet, x='VA', y='Adjusted Draft Score', color='Adjusted Draft Score',
+                         title='NBA 2010-15 Drafts: Floor Impact Counter vs Adjusted Draft Score',
+                         hover_data=(['Player', 'Pos', 'Year', 'FIC', 'Adjusted Draft Score']))
 
-#Run Dash Server
+        fig.update_traces(mode='markers', marker_size=13)
+
+        fig.update_layout(
+            hoverlabel=dict(
+                bgcolor="white",
+                font_size=16,
+                font_family="Rockwell"
+            )
+        )
+
+        fig.update_xaxes(range=[0,15])
+        return fig
+
+
+
+
+
+
+
+
+
+# Run Dash Server
 
 if __name__ == '__main__':
     app.run_server()
+
+
